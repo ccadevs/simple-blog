66db45a83bb8febf6964f994d937df650f297986
Mile S <81876481+ccadevs@users.noreply.github.com>
2024-06-13 11:39:36 +0200

    Update dashboard.php

--- admin/dashboard.php
+++ admin/dashboard.php
@@ -5,7 +5,7 @@
     require_once '../config/database.php';
 
     if (!isset($_SESSION['user_id'])) {
-        header("Location: login.php");
+        header("Location: ./");
         exit();
     }
 
